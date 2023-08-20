9b533647d0ccf0a0eba79019f51fc405565b9bc4
Agamjain12 <agamj990@gmail.com>
2023-08-21 01:20:31 +0530

    Sort folder added

--- src/DSA/HashMap/SortCharactersByFrequency.java
+++ src/DSA/HashMap/SortCharactersByFrequency.java
@@ -21,5 +21,26 @@ public class SortCharactersByFrequency {
 //
 //        System.out.println(sb);
 
+        Map<Character, Integer> agam = new HashMap<>();
+        for (char i : s.toCharArray()) {
+            agam.put(i, agam.getOrDefault(i, 0) + 1);
+        }
+
+        List<Character> list = new ArrayList<>(agam.keySet());
+        Collections.sort(list, (a, b) -> {
+            return (agam.get(a) == agam.get(b))? a - b : agam.get(b) - agam.get(a);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        for (char num : list) {
+            for (int j = 0; j < agam.get(num); j++) {
+                 sb.append(num);
+                 i++;
+            }
+        }
+
+        System.out.println(sb);
+
     }
 }
