c2d2c718a03355852dd3626ca26edf9c86e97bdd
Agamjain12 <agamj990@gmail.com>
2023-08-22 01:23:01 +0530

    Sort folder added

--- src/DSA/IntersectionOfTwoArrays.java
+++ src/DSA/IntersectionOfTwoArrays.java
@@ -1,22 +1,23 @@
 package DSA;
 
-import java.net.Inet4Address;
+
 import java.util.*;
 
 public class IntersectionOfTwoArrays {
     public static void main(String[] args) {
-        int [] arr1 = {2,4,5,4,5,8,10,11};
-        int [] arr2 = {2,2,4,3,4,8,8};
+        int [] nums1 = {2,4,5,4,5,8,10,11};
+        int [] nums2 = {2,2,4,3,4,8,8};
+
 
 
         HashMap<Integer,Integer> agam = new HashMap<>();
         HashMap<Integer,Integer> simo = new HashMap<>();
 
-        for(int i : arr1){
+        for(int i : nums1){
             agam.put(i,agam.getOrDefault(i,0)+1);
         }
 
-        for(int i : arr2){
+        for(int i : nums2){
             simo.put(i,simo.getOrDefault(i,0)+1);
         }
 
@@ -47,10 +48,34 @@ public class IntersectionOfTwoArrays {
            }
 
        }
-        System.out.println(large);
-        System.out.println(Arrays.toString(small));
+
 
 
         System.out.println(Arrays.toString(li.toArray()));
+
+
+
+                            // second method
+
+
+
+        Set<Integer> man = new HashSet<>();
+        for(int i =0; i<nums1.length; i++){
+            for(int j =0; j<nums2.length; j++){
+                if(nums1[i]==nums2[j]){
+                    man.add(nums1[i]);
+                    break;
+                }
+
+            }
+        }
+
+        int[] newarr = new int[man.size()];
+        for(int i =0; i< newarr.length; i++){
+            newarr[i] = (int) man.toArray()[i];
+        }
+
+
+        System.out.println(Arrays.toString(newarr));
     }
 }
--- src/DSA/temo.java
+++ src/DSA/temo.java
@@ -8,22 +8,25 @@ public class temo {
 
 
     public static void main(String[] args) {
-        int[] nums = {5,7,9,2,2,0,0,0,0,4,4};
-        Arrays.sort(nums);
-        Map<Integer,Integer> agam = new HashMap<>();
-        List<Integer> al = new ArrayList<>();
 
-        for(int i : nums){
-            agam.put(i,agam.getOrDefault(i,0)+1);
+        int [] nums1 = {2,4,5,4,5,8,10,11};
+        int [] nums2 = {2,2,4,3,4,8,8};
+
+        Set<Integer> agam = new HashSet<>();
+        for(int i =0; i<nums1.length; i++){
+            for(int j =0; j<nums2.length; j++){
+                if(nums1[i]==nums2[j]){
+                    agam.add(nums1[i]);
+                    break;
+                }
+
+            }
+        }
+         int[] newarr = new int[agam.size()];
+        for(int i =0; i< newarr.length; i++){
+            newarr[i] = (int) agam.toArray()[i];
         }
 
-            for(Map.Entry<Integer,Integer> e : agam.entrySet()){
-//                for(int i=0; i<agam.get(e.getKey()); i++){
-//                    al.add(e.getKey());
-                System.out.println("element: "+ e.getKey()+" frequency: "+e.getValue());
-//                }
-            }
-//        System.out.println(al);
     }
 }
 
